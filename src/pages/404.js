2cf6bbf7cd2c1615d55274459e0fb8c1e38fe97e
F John De Costa <john@mayerfleur.com>
2019-02-10 09:35:13 +1100

    completed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,19 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { PageHeader, Banner } from '../utils/'
+import notFoundImg from '../images/bcg/404Bcg.jpg'
+
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that does not exist... the sadness.</p>
+		<PageHeader img={notFoundImg} min-height= "100vh">
+			<Banner title='404 Not Found' subtitle='You just hit a route that does not exist... the sadness.' />
+		</PageHeader>
+    
   </Layout>
 )
 
 export default NotFoundPage
+
