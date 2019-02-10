9b432a2c95d1e7fa4d47b9e6beef79a80f45c083
F John De Costa <john@mayerfleur.com>
2019-02-10 20:06:18 +1100

    tidied Button.js

--- src/utils/Button.js
+++ src/utils/Button.js
@@ -57,25 +57,6 @@ ${styles.transition({})};
 	cursor: pointer;
 }
 `
-const ReadMoreButton = styled.button`
-display: block;
-color:${styles.colors.mainWhite};
-background: transparent;
-padding: 0.5rem 1rem;
-text-transform: capitalize;
-font-size: 1rem;
-letter-spacing: 0.2rem;
-font-weight: 700px;
-${styles.border({ color: `${styles.colors.mainWhite}` })};
-border-radius: 10px;
-margin-bottom: 1rem;
-${styles.transition({})};
-&:hover{
-	background:${styles.colors.mainBlack};
-	color:${styles.colors.mainYellow};
-	${styles.border({ color: `${styles.colors.mainYellow}` })};
-	cursor: pointer;
-}
-`
 
-export {BannerButton, SectionButton, FooterButton, ReadMoreButton}
+
+export {BannerButton, SectionButton, FooterButton}
