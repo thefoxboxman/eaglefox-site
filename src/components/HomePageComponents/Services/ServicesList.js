b0ee3911e0dcca08b833e6a5035bd333a3b2ec1d
F John De Costa <john@mayerfleur.com>
2019-02-10 16:38:51 +1100

    completed servicesList section

--- src/components/HomePageComponents/Services/ServicesList.js
+++ src/components/HomePageComponents/Services/ServicesList.js
@@ -1,80 +1,106 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import {Section, styles} from '../../../utils'
+import { Section, styles } from '../../../utils'
 
+export function ServicesList() {
+  return (
+    <ServicesSection>
+      <Section>
+        <ServicesListWrapper>
+          <div className="item">
+            <h2>SEO</h2>
+            <p>
+              Get found on Google, Yahoo! and Bing. We follow SEO best
+              practices, ensuring you have the best chance of attracting more
+              customers.
+            </p>
+          </div>
+          <div className="item">
+            <h2>Website Design & Build</h2>
+            <p>
+              Make a great first impression with a hand-crafted website that
+              reflects your professionalism and is built to work for your small
+              business.
+            </p>
+          </div>
 
-export  function ServicesList() {
-	return (
-		<ServicesSection>
-			<Section>
-		<ServicesListWrapper>
-			<div className='item'> 
-			<h2>SEO</h2>
-					<p>Get found on Google, Yahoo! and Bing. We follow SEO best practices, ensuring you have the best chance of attracting more customers.
-		
- </p>
-			</div>
-			<div className='item'>
-					<h2>Website Design</h2>
-					<p>Make a great first impression with a hand-crafted website that reflects your professionalism and is built to work for your small business.
-
-</p>
-			</div>
-			<div className='item'>website build</div>
-			<div className='item'> SEO</div>
-			<div className='item'>web sit design</div>
-			<div className='item'>website build</div>
-		</ServicesListWrapper>
-			</Section>
-		</ServicesSection>
-	)
+          <div className="item">
+            <h2>Social Media Marketing</h2>
+            <p>
+              Running paid advertising on Social Media? You’d better have a well
+              thought out strategy. Facebook and other social media advertising
+              platforms can be an expensive mistake if you are not prepared.
+            </p>
+          </div>
+          <div className="item">
+            <h2>Content Marketing & Copywriting</h2>
+            <p>
+              A form of marketing that involves the creation and sharing of online material (such as videos, blogs, and social media posts) that does not explicitly promote a brand but is intended to stimulate interest in its products or services.
+            </p>
+          </div>
+          <div className="item">
+            <h2>Website Maintenance</h2>
+            <p>
+              There comes a time when a business outgrows their website or the website's platform reaches the end of its life cycle. We will assemble a dedicated project team to rebuild or revamp your platform.
+            </p>
+          </div>
+          <div className="item">
+            <h2>WordPress Sites</h2>
+            <p>
+              Your WordPress website setup and maintained.
+            </p>
+          </div>
+        </ServicesListWrapper>
+      </Section>
+    </ServicesSection>
+  )
 }
 const ServicesSection = styled(Section)`
-	background: ${styles.colors.lightGrey};
-	min-width: 100vw;
-	
+  background: ${styles.colors.lightGrey};
+  min-width: 100vw;
 `
-const ServicesListWrapper = styled.div `
-* {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-}
+const ServicesListWrapper = styled.div`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-*:before, *:after {
-  box-sizing: border-box;
-}
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
 
-
-display: grid;
+  display: grid;
   grid-template-columns: auto;
   grid-row-gap: 1rem;
 
-	.item{
-	background-color: white;
-	border: 2px solid #000;
-	border-radius: 5px;
-	}
+  .item {
+    background-color: white;
+    border: 2px solid #000;
+    border-radius: 5px;
+  }
 
-@media (min-width: 576px) {
+  @media (min-width: 576px) {
     grid-template-columns: 1fr 1fr;
     grid-column-gap: 1rem;
   }
   @media (min-width: 768px) {
     grid-template-columns: repeat(3, 1fr);
-		grid-gap: 2rem;
+    grid-gap: 2rem;
   }
   @media (min-width: 992px) {
     grid-gap: 3rem;
   }
-	h2 {
-		color: ${styles.colors.mainYellow};
-		text-align: center;
-		letter-spacing: 0.1rem;
-	}
-	p{
-		letter-spacing: 0.1rem;
-		line-height: 1.4rem;
-	}
+  h2 {
+    color: ${styles.colors.mainYellow};
+    text-align: center;
+    letter-spacing: 0.1rem;
+  }
+  p {
+    letter-spacing: 0.1rem;
+    line-height: 1.4rem;
+    padding: 1px 5px 1px 10px;
+  }
 `
