37166a52aaf13b99439208bc5cb04ca891e8542c
F John De Costa <john@mayerfleur.com>
2019-02-11 15:28:38 +1100

    gitignore .Original images

--- src/pages/AboutSkills.js
+++ src/pages/AboutSkills.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 
 import AboutSkillsText from '../components/AboutPageComponents/AboutSkillsText'
-import { PageHeader, Banner } from '../utils/'
-import aboutImg from '../images/bcg/aboutBcg.jpg'
+import { PageHeader, Banner } from '../utils'
+import aboutSkillsImg from '../images/bcg/aboutskillsBcg.jpg'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -11,7 +11,7 @@ import SEO from '../components/seo'
 const AboutSkillsPage = () => (
 	<Layout>
 		<SEO title="AboutSkills" keywords={[`gatsby`, `application`, `react`]} />
-		<PageHeader img={aboutImg}>
+		<PageHeader img={aboutSkillsImg}>
 			<Banner title='my skills' subtitle='the good oil' />
 		</PageHeader>
 		<AboutSkillsText />
--- src/utils/ReadMoreButton.js
+++ src/utils/ReadMoreButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FaPhone } from 'react-icons/fa'
+
 
 import { styles } from '../utils'
 
