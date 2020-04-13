2c6072689627f10e0fd047eb3f2bd64629561344
John <john@John-New-MacPro.local>
2020-04-13 17:16:46 +1000

    Edited header button

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,38 +2,34 @@ import React from 'react'
 
 import { Link } from 'gatsby'
 
-import { HomeHeader, Banner, BannerButton } from '../utils'
+import { HomeHeader, Banner, SectionButton } from '../utils'
 
 import homeImg from '../images/bcg/homeBcg.jpg'
 import QuickInfo from '../components/HomePageComponents/QuickInfo'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Blurb from "../components/HomePageComponents/Blurb.js"
-
-
-
+import Blurb from '../components/HomePageComponents/Blurb.js'
 
 const IndexPage = () => (
-	<Layout>
-	
-		<SEO title="Home"  />
-		
-		<HomeHeader img={homeImg}>
-			
-			<Banner title="Everything Web" subtitle="Helping Your business go from ok... to GREAT!" >
-				<Link to="/services/" style={{ textDecoration: 'none' }}>
-					<BannerButton style={{ margin: '2rem auto' }}>Services</BannerButton>
-				</Link>
-			</Banner>
-			
-		</HomeHeader>
-		<Blurb />
-		<QuickInfo />
-		
+  <Layout>
+    <SEO title="Home" />
 
-	</Layout>
+    <HomeHeader img={homeImg}>
+      <Banner
+        title="Everything Web"
+        subtitle="Helping Your business go from good... to GREAT!"
+      >
+        <Link to="/services/" style={{ textDecoration: 'none' }}>
+          <SectionButton style={{ margin: '2rem auto' }}>
+            See Our Services
+          </SectionButton>
+        </Link>
+      </Banner>
+    </HomeHeader>
+    <Blurb />
+    <QuickInfo />
+  </Layout>
 )
-
 
 export default IndexPage
