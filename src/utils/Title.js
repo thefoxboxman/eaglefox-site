311b2128f445412ffdc1ede65a9ca5e1851d2c9e
John <john@John-New-MacPro.local>
2020-04-19 17:13:21 +1000

    mainyellow2 to mainyellow to improve contrast

--- src/utils/Title.js
+++ src/utils/Title.js
@@ -1,39 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import {styles} from '../utils'
+import { styles } from '../utils'
 
-export function Title({title,message}) {
-	return (
-		<TitleWrapper>
-		<h3 className="message">{message}</h3>
-		<h1 className="title">{title}</h1>
-		<div className="underline"></div>	
-		</TitleWrapper>
-	)
+export function Title({ title, message }) {
+  return (
+    <TitleWrapper>
+      <h3 className="message">{message}</h3>
+      <h1 className="title">{title}</h1>
+      <div className="underline"></div>
+    </TitleWrapper>
+  )
 }
 
 Title.defaultProps = {
-	message: 'our message',
-	title:'our title',
+  message: 'our message',
+  title: 'our title',
 }
 const TitleWrapper = styled.div`
   text-align: center;
   .message {
-    
     ${styles.textSlanted};
     letter-spacing: 0.3rem;
-    color: ${styles.colors.mainYellow2};
+    color: ${styles.colors.mainYellow};
   }
   .title {
-		letter-spacing: 0.3rem;
-		font-size: 2rem;
-		text-transform: uppercase;	
-	}
-	.underline{
-		width: 5rem;
-		height: 0.2rem;
-		background: ${styles.colors.mainYellow2};
-		margin: 0.5rem auto;
-	}
+    letter-spacing: 0.3rem;
+    font-size: 2rem;
+    text-transform: uppercase;
+  }
+  .underline {
+    width: 5rem;
+    height: 0.2rem;
+    background: ${styles.colors.mainYellow};
+    margin: 0.5rem auto;
+  }
 `
